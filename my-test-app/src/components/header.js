712fec249a08f58edc7a14c6f9035aa4a04efdc9
yoshinagamasahiro <yoshinagamasahiro@team-lab.com>
2022-10-23 16:54:19 +0900

    フォームと遊びでボタンを２箇所追加

--- my-test-app/src/components/header.js
+++ my-test-app/src/components/header.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from './logo.svg';
 
 function AppHeader() {
+  // カウントアップ追加
+  const [cource, setcource] =useState(0)
+  const test=()=>{
+    setcource(cource+2)
+  }
+
+  const test_2 =()=>{
+    alert("COMMONN!!!")
+  }
+
+  //フォームの詳細仕様
+  const [name, setName] = useState({
+    lastName: '',
+    firstName: ''
+  })
+
     return(
         <>
         <header className="App-header">
@@ -9,6 +25,18 @@ function AppHeader() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <label>{cource}</label>
+        <button onClick={test}>SWITCH COLOR</button>
+        <button onClick={test_2}>ADD TO CART</button>
+        <form>
+        <input placeholder="姓"
+          value={name.lastName}
+          onChange={(e)=>setName({name,lastName: e.target.value})}></input>
+        <input placeholder="名"
+          value={name.firstName}
+          onChange={(e)=>{setName({name,firstName: e.target.value})}}></input>
+        <input type="submit" value="送信" ></input>
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
